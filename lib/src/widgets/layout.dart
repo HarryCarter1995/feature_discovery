4d086d511cf24c93be09e36b19fd57c52df3b3c5
HarryCarter1995 <70072521+HarryCarter1995@users.noreply.github.com>
2022-07-24 13:44:06 +0100

    Update layout.dart

--- lib/src/widgets/layout.dart
+++ lib/src/widgets/layout.dart
@@ -10,7 +10,7 @@ class CenterAbout extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Positioned(
         top: position!.dy,
-        left: position!.dx + 50,
+        left: position!.dx - 200,
         child: FractionalTranslation(
           translation: const Offset(-0.5, -0.5),
           child: child,
